64a870369181f35f8becef60ea87d028f684c4eb
thecoldestm0ment <embery@naver.com>
2026-01-17 23:08:33 +0900

    Initial commit

---
+++ src/main/java/com/anonLove/anonLove/AnonLoveApplication.java
@@ -0,0 +1,13 @@
+package com.anonLove.anonLove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnonLoveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnonLoveApplication.class, args);
+	}
+
+}
